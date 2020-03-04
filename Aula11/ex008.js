7a2625ef401fda6d3f309602f2c6fb2e0750f904
Menacho <Arlissonmenacho@gmail.com>
2020-03-04 17:39:06 -0400

    Aula 11
    utilizando if, else, function.

--- Aula11/ex008.js
+++ Aula11/ex008.js
@@ -1,6 +1,7 @@
-var vel = 60.5
+var vel = 12
 console.log(`A velocidade do seu carro é ${vel} Km/h`)
-if(vel >60){
-    console.log(`Você ultrapassou a velocidade permitida.`)
-}
+    if(vel >60)
+    {
+        console.log(`Você ultrapassou a velocidade permitida.`)
+    }
 console.log(`Dirija sempre usando cinto de segurança!`)
