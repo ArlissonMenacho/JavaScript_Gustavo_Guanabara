87da3e10e149a25fe7b62af2f08085c6e47bab89
Menacho <Arlissonmenacho@gmail.com>
2020-03-07 19:11:03 -0400

    Aulas 16 e execercio 18
    Finalização do Curso JavaScript com Gustavo Guanabara

---
+++ Aula16/funcao01.js
@@ -0,0 +1,21 @@
+/* function parimpar(n){
+    if(n%2 ==0){
+        return 'Par!'
+    }else{
+        return 'Impar!'
+    }
+}
+
+
+/* let res = parimpar(9)
+console.log(res) */
+
+/* console.log(parimpar(3))
+
+console.log(`O valor do número citado é ${parimpar(224)}`) */
+
+
+function soma(n1=0 , n2=0 ){
+    return n1 + n2
+}
+console.log(soma(0,8))
